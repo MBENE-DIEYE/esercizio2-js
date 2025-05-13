99a5b44a46096bed4db2499b41bec2185c983e39
mbene dieye <mbenedieye757@gmail.com>
2025-05-13 22:50:10 +0200

    sotto classe electrica

--- script.js
+++ script.js
@@ -35,3 +35,27 @@ let auto2 = Automobile.restruczione(auto1);
 let auto3 = Automobile.aggiungichilometraggio(auto1)
  
 console.log(auto3)
+
+// sotto classe Electrica
+
+class Eletrica extends Automobile{
+    autonomia =0
+
+    constructor(marca,modello,anno,chilometraggio,autonomia){
+        super(marca,modello,anno,chilometraggio)
+        this.autonomia = autonomia
+    }
+   static restruczione(automobile){
+        let auto = new Eletrica (automobile.marca,automobile.modello,automobile.anno,automobile.chilometraggio,automobile.autonomia )
+
+        return auto
+    }
+
+    ricarica(km){
+        return km +1
+    }
+}
+
+let auto4 = new Eletrica ("audi","renault",25,70+"km",100);
+let auto5 = Eletrica.restruczione(auto4);
+console.log(auto5)
