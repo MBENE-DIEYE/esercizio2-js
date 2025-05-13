62f8316abc09c1d61b64aa2dfad5b41bb4a42c70
mbene dieye <mbenedieye757@gmail.com>
2025-05-13 22:29:22 +0200

    aggiungi methodi & proprietà

--- script.js
+++ script.js
@@ -2,20 +2,36 @@ class Automobile{
     marca = "";
     modello = "";
     anno = 0
+    // aggiungi propietà
+    chilometraggio =""
 
-    constructor(marca,modello,anno){
+    constructor(marca,modello,anno,chilometraggio){
         this.marca = marca;
         this.modello = modello;
         this.anno = anno;
+        this.chilometraggio = chilometraggio
     }
 
   static restruczione(automobile){
-        let auto = new Automobile (automobile.marca,automobile.modello,automobile.anno)
+        let auto = new Automobile (automobile.marca,automobile.modello,automobile.anno )
 
         return auto
     }
+    // aggiungi methodi
+static aggiungichilometraggio(km){
+    let chilomet = new Automobile(km.marca,km.modello,km.anno,km.chilometraggio)
+
+    return chilomet 
 }
 
-let auto1 = new Automobile("audi","renault",13);
+static mostrachilometraggio (){
+    return aggiungichilometraggio(km)
+}
+
+}
+
+let auto1 = new Automobile("audi","renault",13,40+"km");
 let auto2 = Automobile.restruczione(auto1);
-console.log(auto2)
+let auto3 = Automobile.aggiungichilometraggio(auto1)
+ 
+console.log(auto3)
